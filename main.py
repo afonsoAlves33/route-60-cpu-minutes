5050caf168f17545b792855cd490a4965ce0c922
afonsoAlves33 <afonso.alves161110@gmail.com>
2024-10-22 10:32:15 -0300

    makes the route response to be streamed, so that reverse proxies do not generate a 502 error for time out

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
+from starlette.responses import StreamingResponse
 import asyncio
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def index():
     return "Main route: /wait_60_minutes"
-
-@app.get("/wait_60_minutes")
 async def wait_60_minutes():
     try:
         for i in range(0, 3600):
             print("Seconds: ",i)
             await asyncio.sleep(1)
+            yield f"{i}\n"
     except KeyboardInterrupt:
         raise SystemExit()
-    return "Time awaited"
+
+
+@app.get("/wait_60_minutes")
+async def streaming_response():
+    return StreamingResponse(wait_60_minutes(), media_type="text/plain")
+
+
+
+
